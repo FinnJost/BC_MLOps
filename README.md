2d991e53330cb8b69125441408dc36651a19062c
FinnJost <finnbjost@gmx.de>
2025-05-04 13:20:55 +0000

    Pending changes exported from your codespace

--- README.md
+++ README.md
@@ -54,19 +54,55 @@ see the git commits
 - add dependencies from day 1: 'uv add scikit-learn==1.2.2 numpy==1.26.4'
 - add flask: 'uv add flask'
 - add pytest 'uv add pytest'
+- add requests: 'uv add --dev requests'
 - copy over model from day 1
+- add loguru
 
 
+### ping example
+- can be run via 'uv run python src/duration_pred_serve/ping.py'
+- open in browser: 'http://127.0.0.1:9696/ping'
+    also: 'curl 127.0.0.1:9696/ping'
+
+### implement serve 
+- run it via 'uv run python src/duration_pred_serve/serve.py'
+- or via requests: 'uv run python duration-tests/predict-test.py'
+
+- run via makefile:
+    - make run
+    - make predict-test 
+
+### use environment variables 
+- have a look into export
+- on the python side use os.getenv
 
 
+### add loggong with loguru 
+- added via 'uv add loguru'
+- use via 'from loguru import logger' 
 
 
+### use docker 
+add commands to the makefile: 
+    - 'make docker_build'
+    - 'make docker_run'
+
+### gunicorn -> for production
+- uv add gunicorn
+- run via 'uv run gunicorn --bind=0.0.0.0:9696 src.duration_pred_serve.serve:app' but we just update the entrypoint
 
 
-
-
-
-
+### fly.io for deployment 
+- account 
+- install flyctl: 'curö -L https://fly.io/install.sh | sh'
+- run 
+'''
+export FLYCTL_INSTALL="/home/codespace/.fly"
+export PATH="$FLYCTL_STALL/bin:$PATH"
+'''
+- also copy these to ~/.bashrc 
+- run 'flyctl launch'
+- put new flyio url into predict-test.py and run 'make predict_test'
 
 
 
--- day_2/duration_pred_serve/src/duration_pred_serve/serve.py
+++ day_2/duration_pred_serve/src/duration_pred_serve/serve.py
@@ -1,12 +1,70 @@
 import pickle 
-with open('./models/2022-01.bin', 'rb') as f_in:
+from flask import Flask, request, jsonify
+import os
+from loguru import logger 
+
+
+MODEL_PATH = os.getenv('MODEL_PATH', 'model.bin')
+VERSION = os.getenv('VERSION', 'not defined')
+
+
+logger.info(f'Using model at: {MODEL_PATH}')
+logger.info(f'Version: {VERSION}')
+
+
+with open(MODEL_PATH, 'rb') as f_in:
     model = pickle.load(f_in)
 
-trip = {
-    'PULocationID': '43', 
-    'DOLocationID': '238', 
-    'trip_distance': 1.16, 
-}
 
-prediction = model.predict(trip)
-print(prediction[0])
+
+# trip = {
+#     'PULocationID': '43', 
+#     'DOLocationID': '238', 
+#     'trip_distance': 1.16, 
+# }
+
+# prediction = model.predict(trip)
+# print(prediction[0])
+
+
+# ''feature engeneering''
+def prepare_features(ride):
+    features = dict()
+    features['PULocationID'] = str(ride['PULocationID'])
+    features['DOLocationID'] = str(ride['DOLocationID'])
+    features['trip_distance'] = float(ride['trip_distance'])
+    ## here other crsions could happen as feature preparation
+
+    return features
+
+def predict(features):
+    preds = model.predict(features)
+
+    return float(preds[0])
+
+
+app = Flask('duration-prediction')
+
+@app.route('/version', methods=['GET'])
+def version():
+    return VERSION
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    prediction = predict(features)
+
+    result = {
+        'prediction': {'duration': prediction} ,
+        'version': VERSION, 
+    }
+    logger.info(f'processed: {ride}, result: {prediction}')
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
+
